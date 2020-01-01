a52df5be60352d31409055d52876820b803516c2
SSJDeathSpawn <adikesavan28@gmail.com>
2020-01-01 14:25:30 +0530

    Initial commit

---
+++ src/main/java/mod/whackspawn/anything/AnythingMod.java
@@ -0,0 +1,27 @@
+package mod.whackspawn.anything;
+
+import mod.whackspawn.anything.lib.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.MODVER)
+public class AnythingMod {
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public static void Init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
---
+++ src/main/java/mod/whackspawn/anything/lib/Reference.java
@@ -0,0 +1,9 @@
+package mod.whackspawn.anything.lib;
+
+public class Reference {
+
+    public static final String MODID = "anything";
+    public static final String MODNAME = "Anything Mod";
+    public static final String MODVER = "1.12.2-0.1.0";
+
+}
